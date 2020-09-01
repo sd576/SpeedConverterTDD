eeb9849ea309dfcaeab4b41c103624341d3cea37
Stuart du Casse <sd576@btinternet.com>
2020-09-01 12:42:56 +0100

    refactor

--- SpeedConverter/SpeedConverter.Tests/SpeedConversionServiceTests.cs
+++ SpeedConverter/SpeedConverter.Tests/SpeedConversionServiceTests.cs
@@ -1,21 +1,47 @@
 using SpeedConverter.Code;
+using System;
 using Xunit;
 
 namespace SpeedConverter.Tests
 {
-    public class SpeedConversionServiceTests
+    public class SpeedConversionServiceTests : IDisposable
     {
+        private readonly SpeedConversionService speedConverter;
+
+        public SpeedConversionServiceTests()
+        {
+            speedConverter = new SpeedConversionService();
+        }
+
         [Fact]
         public void ConvertToMilesPerHour_0_0()
         {
-            // arrange
-            var speedConverter = new SpeedConversionService();
-
             // act
             var milesPerHour = speedConverter.ConvertToMilesPerHour(0);
 
             // assert
             Assert.Equal(0, milesPerHour);
+        }
+
+        [Fact]
+        public void ConvertToMilesPerHour_10_6()
+        {
+            var milesPerHour = speedConverter.ConvertToMilesPerHour(10);
+
+            Assert.Equal(6, milesPerHour);
+        }
+
+        [Fact]
+        public void ConvertToMilesPerHour_11_7()
+        {
+            var milesPerHour = speedConverter.ConvertToMilesPerHour(11);
+
+            Assert.Equal(7, milesPerHour);
+        }
+
+        public void Dispose()
+        {
+            // no-op
         }
     }
 }
